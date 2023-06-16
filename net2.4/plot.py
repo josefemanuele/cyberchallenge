d85dc5e5e96db5791a1a6e983e4a08a5af966a53
Josef Emanuele Zerpa Ruiz <josefzerpa@outlook.com>
2023-06-16 09:06:09 -0400

    remove old files

--- net2.4/plot.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-with open('draw.txt') as file:
-    for line in file.readlines():
-        command = line.strip().split(',')
-        start = [command[3], command[4]]
-        end = [command[5], command[6]]
-        plt.plot(start, end)
-    plt.show()
--- net2.5/r.js
+++
@@ -1,6 +0,0 @@
-function r(s, i) {
-  return s.replace(/[a-zA-Z]/g, function (c) {
- return String.fromCharCode((c <= 'Z' ? 90 : 122) >= (c = c.charCodeAt(0) + i) ? c : c - 26);
- });
-}
-
